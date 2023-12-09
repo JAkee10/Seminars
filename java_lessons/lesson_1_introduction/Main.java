70c4790973b5d06d69b34510918b6dc07b7eaa1c
JAkee <JakePomg94@gmail.com>
2023-12-10 00:46:30 +0200

    CHANGED: main folder's name to java_lessons

---
+++ java_lessons/lesson_1_introduction/Main.java
@@ -0,0 +1,11 @@
+package java_lessons.lesson_1_introduction;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
